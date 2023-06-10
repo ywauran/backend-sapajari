abeac71047e2690d2266f888e62b60625b78a00b
Yohanes Harke Wauran <harkewauran23@gmail.com>
2023-06-10 15:50:27 +0800

    change config

--- config/Database.js
+++ config/Database.js
@@ -1,7 +1,7 @@
 import { Sequelize } from "sequelize";
 
-const db = new Sequelize("db_sapajari", "root", "", {
-  host: "localhost",
+const db = new Sequelize("db_sapajari", "root", "123456", {
+  host: "34.101.197.80",
   dialect: "mysql",
 });
 
